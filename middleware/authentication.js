d0d42a7156dfe4b43f569287766e1860299c0379
timothy <effiongtimothy1@gmail.com>
2024-01-01 00:45:23 +0100

    authenticate token update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,42 +1,51 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function authenticateToken(req, res, next) {
-    const token = req.header('Authorization');
+  const authHeader = req.header("Authorization");
+  console.log("recieved Token " + authHeader);
 
-    if (!token) {
-        return res.status(401).json({ message: 'Access denied - No token provided' });
+  if (!authHeader) {
+    return res
+      .status(401)
+      .json({ message: "Access denied - No token provided" });
+  }
+  
+//   const [bearer, token] = authHeader.split(' ');
+
+//   if (bearer !== 'Bearer' || !token) {
+//     return res.status(401).json({ message: 'Invalid token format' });
+//   }
+
+  try {
+    const secretKey = process.env.JWT_SECRET;
+    const decoded = jwt.verify(authHeader, secretKey);
+
+    if (decoded.exp < Date.now() / 1000) {
+      return res.status(401).json({ message: "Token has expired" });
     }
 
-    try {
-        const secretKey = process.env.JWT_SECRET;
-        const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error(error);
 
-        if (decoded.exp < Date.now() / 1000) {
-            return res.status(401).json({ message: 'Token has expired' });
-        }
-
-        req.user = decoded;
-        next(); 
-
-    } catch (error) {
-        console.error(error);
-
-        if (error instanceof jwt.JsonWebTokenError) {
-            // Invalid token format or signature
-            console.error('Invalid token format or signature:', error.message);
-            return res.status(401).json({ message: 'Invalid token format or signature' });
-        } else if (error instanceof jwt.TokenExpiredError) {
-            // Token has expired
-            console.error('Token has expired:', error.message);
-            return res.status(401).json({ message: 'Token has expired' });
-        } else {
-            // Other errors
-            console.error('Error verifying token:', error.message);
-            return res.status(401).json({ message: 'Invalid token' });
-        }
+    
+    if (error instanceof jwt.TokenExpiredError) {
+        console.error("Token has expired:", error.message);
+        return res.status(401).json({ message: "Token has expired" });
+      } else if (error instanceof jwt.JsonWebTokenError) {
+      console.error("Invalid token format or signature:", error.message);
+      return res
+        .status(401)
+        .json({ message: "Invalid token format or signature" });
+    } 
+    else {
+      console.error("Error verifying token:", error.message);
+      return res.status(401).json({ message: "Invalid token" });
     }
+  }
 }
- module.exports = {
-    authenticateToken,
+module.exports = {
+  authenticateToken,
 };
